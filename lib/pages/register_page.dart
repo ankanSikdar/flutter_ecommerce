a9f7b4769ea7b15de1ad6df876dcfd157e48dc2d
Ankan Sikdar <ankansikdar@gmail.com>
2020-09-28 16:49:28 +0530

    Refactored the widget tree

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -7,6 +7,88 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  Widget _showTitle() {
+    return Text(
+      'Register',
+      style: Theme.of(context).textTheme.headline5,
+    );
+  }
+
+  Widget _showUsernameInput() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Username',
+          icon: FaIcon(FontAwesomeIcons.userAlt),
+          border: OutlineInputBorder(),
+          hintText: 'Enter username, min length 6',
+        ),
+        keyboardType: TextInputType.text,
+      ),
+    );
+  }
+
+  Widget _showEmailInput() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Email',
+          icon: FaIcon(FontAwesomeIcons.solidEnvelope),
+          border: OutlineInputBorder(),
+          hintText: 'Enter your email id',
+        ),
+        keyboardType: TextInputType.emailAddress,
+      ),
+    );
+  }
+
+  Widget _showPasswordInput() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+          labelText: 'Password',
+          icon: FaIcon(FontAwesomeIcons.key),
+          border: OutlineInputBorder(),
+          hintText: 'Create a password',
+        ),
+      ),
+    );
+  }
+
+  Widget _showFormActionsInput() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          RaisedButton(
+            onPressed: () {},
+            elevation: 8,
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Text(
+              'SignUp',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText2
+                  .copyWith(color: Colors.black),
+            ),
+            color: Theme.of(context).primaryColor,
+          ),
+          FlatButton(
+            onPressed: () {},
+            child: Text('Existing User? Login',
+                style: Theme.of(context).textTheme.bodyText2),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,74 +102,11 @@ class _RegisterPageState extends State<RegisterPage> {
           child: Form(
             child: Column(
               children: [
-                Text(
-                  'Register',
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Username',
-                      icon: FaIcon(FontAwesomeIcons.userAlt),
-                      border: OutlineInputBorder(),
-                      hintText: 'Enter username, min length 6',
-                    ),
-                    keyboardType: TextInputType.text,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      icon: FaIcon(FontAwesomeIcons.solidEnvelope),
-                      border: OutlineInputBorder(),
-                      hintText: 'Enter your email id',
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      icon: FaIcon(FontAwesomeIcons.key),
-                      border: OutlineInputBorder(),
-                      hintText: 'Create a password',
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: Column(
-                    children: [
-                      RaisedButton(
-                        onPressed: () {},
-                        elevation: 8,
-                        padding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Text(
-                          'SignUp',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText2
-                              .copyWith(color: Colors.black),
-                        ),
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      FlatButton(
-                        onPressed: () {},
-                        child: Text('Existing User? Login',
-                            style: Theme.of(context).textTheme.bodyText2),
-                      )
-                    ],
-                  ),
-                )
+                _showTitle(),
+                _showUsernameInput(),
+                _showEmailInput(),
+                _showPasswordInput(),
+                _showFormActionsInput(),
               ],
             ),
           ),
